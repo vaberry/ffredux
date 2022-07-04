dc96a7f3a2bfb74d6679fdb6d292f7b7be49da0f
vaberry <vincent.berry11@gmail.com>
2022-07-04 14:37:57 -0400

    1

--- ffredux/settings.py
+++ ffredux/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # DEBUG = os.getenv("DEBUG",False) == True
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","127.0.0.1,localhost").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS","sea-turtle-app-e659n.ondigitalocean.app","127.0.0.1,localhost").split(",")
 
 
 # Application definition
