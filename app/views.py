75018952e0fd41c138ffb2c784459279e320e2b5
vaberry <vincent.berry11@gmail.com>
2022-07-04 20:23:29 -0400

    1

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+class Home(View):
+    def get(self,request,*args,**kwargs):
+        return render(request,'app/base.html')
--- ffredux/settings.py
+++ ffredux/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'ffredux.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'mysite/templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'app/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
