12cbfda3bf3bdb65a03d4a44b8d5cfa965680e48
vaberry <vincent.berry11@gmail.com>
2022-07-06 15:44:15 -0400

    1

--- ffredux/settings.py
+++ ffredux/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 
 # import environ
 # env = environ.Env()
-
+#TEST
 AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 
